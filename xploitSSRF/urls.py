5655669ed8808b5a2ef350c8b021bad54793b8cb
pavanw3b <pavanw3b@gmail.com>
2022-08-22 10:51:39 +0530

    Backing up for - still experimenting with gopher

---
+++ xploitSSRF/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.static import static
+from django.urls import path
+from django.conf import settings
+from xploitPickl import views
+
+urlpatterns = [
+    path('', views.index),
+    path('dashboard', views.dashboard),
+    path('preview', views.preview)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
